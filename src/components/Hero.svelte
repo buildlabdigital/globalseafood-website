13f412d1676949274c42ab514f3f29667e4619c8
Mike Williams <mwilliams1188@gmail.com>
2021-11-17 13:14:22 -0500

    latest changes

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -4,6 +4,8 @@
   let tagline = `Buy and sell the world's best seafood`;
   let subTagline = `Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.`;
   let contactEmail = "jon@globalseafoodexchange.com";
+  let appleLink = "/";
+  let googleLink = "/";
 </script>
 
 <!--
@@ -29,7 +31,8 @@
     >
       <div>
         <div>
-          <img class="h-11 w-auto" src={logo} alt="Global Seafood Exchange" />
+          <!-- <img class="h-11 w-auto" src={logo} alt="Global Seafood Exchange" /> -->
+          <span class="font-bold text-2xl">Global Seafood Exchange</span>
         </div>
         <div class="mt-20">
           <div>
@@ -42,7 +45,7 @@
               <span
                 class="inline-flex items-center text-sm font-medium text-indigo-600 space-x-1"
               >
-                <a href="mailto={contactEmail}">Contact</a>
+                <a href="mailto:{contactEmail}">Contact</a>
                 <!-- Heroicon name: solid/chevron-right -->
                 <svg
                   class="h-5 w-5"
@@ -70,7 +73,7 @@
               {subTagline}
             </p>
           </div>
-          <form action="#" class="mt-12 sm:max-w-lg sm:w-full sm:flex">
+          <!-- <form action="#" class="mt-12 sm:max-w-lg sm:w-full sm:flex">
             <div class="min-w-0 flex-1">
               <label for="hero-email" class="sr-only">Email address</label>
               <input
@@ -87,77 +90,26 @@
                 >Notify me</button
               >
             </div>
-          </form>
-          <div class="mt-6">
-            <div class="inline-flex items-center divide-x divide-gray-300">
-              <div class="flex-shrink-0 flex pr-5">
-                <!-- Heroicon name: solid/star -->
-                <svg
-                  class="h-5 w-5 text-yellow-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                  />
-                </svg>
-                <!-- Heroicon name: solid/star -->
-                <svg
-                  class="h-5 w-5 text-yellow-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                  />
-                </svg>
-                <!-- Heroicon name: solid/star -->
-                <svg
-                  class="h-5 w-5 text-yellow-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                  />
-                </svg>
-                <!-- Heroicon name: solid/star -->
-                <svg
-                  class="h-5 w-5 text-yellow-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                  />
-                </svg>
-                <!-- Heroicon name: solid/star -->
-                <svg
-                  class="h-5 w-5 text-yellow-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                  />
-                </svg>
-              </div>
+          </form> -->
+          <div class="mt-6 mx-auto">
+            <div class="flex items-center divide-x divide-gray-300">
               <div
-                class="min-w-0 flex-1 pl-5 py-1 text-sm text-gray-500 sm:py-3"
+                class="min-w-0 flex-1 flex items-center justify-center sm:justify-start py-1 text-sm text-gray-500 sm:py-3"
               >
-                <span class="font-medium text-gray-900">Rated 5 stars</span> by
-                over
-                <span class="font-medium text-indigo-600">500 beta users</span>
+                <a href={appleLink} class="flex justify-center">
+                  <img
+                    class="h-20 py-4 w-auto"
+                    src="/app_store_black.svg"
+                    alt="Apple app store"
+                  />
+                </a>
+                <a href={googleLink} class="h-20 py-1 flex justify-center">
+                  <img
+                    class="h-full"
+                    src="/google-play-badge.png"
+                    alt="Google Play store"
+                  />
+                </a>
               </div>
             </div>
           </div>
