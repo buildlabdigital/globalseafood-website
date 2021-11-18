929f2bdcee22a7ff02ac121d218a8ae214edd2a8
Jon Thornham <jon@thornham.ca>
2021-11-17 22:08:49 -0800

    Updated colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,36 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 const config = {
-	mode: 'jit',
-	purge: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ['Inter var', ...defaultTheme.fontFamily.sans]
-			}
-		}
-	},
-	plugins: []
+  mode: "jit",
+  purge: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
 };
 
 module.exports = config;
+
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        gse: {
+          pacificBlue: "#002F3B",
+          pacificBlueAccent: "#1D454F",
+          pacificBlueMessage: "#009DC4",
+          pacificGrey: "#909094",
+          pacificGreen: "#34C759",
+          pacificRed: "#FF3B30",
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
