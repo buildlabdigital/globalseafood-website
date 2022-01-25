a3c3cb5ebd17c35505b8d92fb4c794411529763f
Jon Thornham <jon@thornham.ca>
2022-01-25 10:13:15 -0800

    Updated the text on the site.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -21,63 +21,63 @@
   imageSide="left"
   imageUrl="/BuyDetail.png"
   title="Create Buy & Sell Listings"
-  description="Easily create buy and sell listings...Easily create buy and sell listings...Easily create buy and sell listings...Easily create buy and sell listings...Easily create buy and sell listings...Easily create buy and sell listings...Easily create buy and sell listings...Easily create buy and sell listings..."
+  description="Easily create Buy and Sell type listings to make buying and selling seafood easy. Want to sell seafood? Create a Buy listing. Want to buy seafood? Create a Sell Listing."
 />
 
 <FeatureSection
   imageSide="right"
   imageUrl="/SellOfferDetail.png"
-  title="Sell or Offer"
-  description="Easily create buy and sell listings...Easily create buy and sell listings...Easily create buy and sell listings...Easily create buy and sell listings...Easily create buy and sell listings...Easily create buy and sell listings...Easily create buy and sell listings...Easily create buy and sell listings..."
+  title="Simple Pricing & Payments"
+  description="Pricing should be simple. The seller pays $0.04 USD/lbs regardless of seafood type or location. Payment is simplied uisng a single wire payment. We route your payment to the seller to make purchasing easy."
 />
 
 <FeatureSection
   imageSide="left"
   imageUrl="/MakeOffer.png"
   title="Make Offers"
-  description="Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer..."
+  description="If you'd like to allow buyers or sellers to suggest a price you can add Offers to your listing. You set a base price in your listing but Offers give buyers and seller the option to suggest another price."
 />
 
 <FeatureSection
   imageSide="right"
   imageUrl="/Create.png"
-  title="Create Listings"
-  description="Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer..."
+  title="Creating Listings"
+  description="Creating listings is easy. Select the seafood you want to sell, list how much you have to sell, set your price, add a description, upload pictures and list it in the marketplace."
 />
 
 <FeatureSection
   imageSide="left"
   imageUrl="/Filter.png"
   title="Quickly Filter"
-  description="Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer..."
+  description="Quickly filter the listings in the marketplace to make buying and selling easy. If you do not see what you are looking for then create a listing for other to view."
 />
 
 <FeatureSection
   imageSide="right"
   imageUrl="/History.png"
   title="Manage Orders, Offers, & Listings"
-  description="Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer..."
+  description="Easily manage your orders, offers and listings in one central location. Make payments, accept or reject offers and manage your listings."
 />
 
 <FeatureSection
   imageSide="left"
   imageUrl="/Messenger.png"
   title="Built In Messenger"
-  description="Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer..."
+  description="Our built in messenger makes communicating with buyers and sellers easy. Send text, picture, and video messages to help market your product and create piece of mind for buyers."
 />
 
 <FeatureSection
   imageSide="right"
   imageUrl="/Currencies.png"
   title="38 Currencies"
-  description="Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer..."
+  description="Buy and Sell in the currency that makes sense for your business. When creating a listing select one of the following currencies. AED, AUD, BGN, BHD, CAD, CHF, CNY, CZK, DKK, EUR, GBP, HKD, HRK, HUF, IDR, ILS, INR, JPY, KES, KWD, MXN, MYR, NOK, NZD, OMR, PHP, PLN, QAR, RON, RUB, SAR, SEK, SGD, THB, TRY, UGX, USD, ZAR"
 />
 
 <FeatureSection
   imageSide="left"
   imageUrl="/Account.png"
   title="Account Management"
-  description="Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer... Easliy Sell or Offer..."
+  description="Manage your account in one simple locaiton. Profile image, email address, business addresses, bank accounts, etc."
 />
 
 <Footer />
