740a85ed1f511eac6c0e84e3fa07410b4f0c3e9c
Jon Thornham <jon@thornham.ca>
2022-02-21 21:06:29 -0800

    Privacy Policy updated 2

--- src/routes/privacy.svelte
+++ src/routes/privacy.svelte
@@ -1,13 +1,14 @@
 <script context="module">
+  import UnorderedList from "./UnorderedList.svelte";
   export const prerender = true;
 </script>
 
 <svelte:head>
-  <title>About</title>
+  <title>Privacy</title>
 </svelte:head>
 
-<div class="mt-6">
-  <h2 class="text-3xl font-extrabold tracking-tight text-white">
+<div class="mt-6 p-8">
+  <h2 class="text-3xl font-extrabold tracking-tight text-white pt-8">
     Privacy Policy
   </h2>
   <p class="mt-4 text-lg text-white">
@@ -15,18 +16,8 @@
     Please take a few minutes to read the following document to learn more about
     our privacy policy.
   </p>
-  <h2 class="text-3xl font-extrabold tracking-tight text-white">
-    Privacy Policy
-  </h2>
-  <p class="mt-4 text-lg text-white">
-    Global Seafood Exchange LLC (GSE) is committed to protecting your privacy.
-    Please take a few minutes to read the following document to learn more about
-    our privacy policy.
-  </p>
-</div>
 
-<div>
-  <h2 class="text-3xl font-extrabold tracking-tight text-white">
+  <h2 class="text-3xl font-extrabold tracking-tight text-white pt-8">
     Privacy Policy
   </h2>
   <p class="mt-4 text-lg text-white">
@@ -34,29 +25,29 @@
     Please take a few minutes to read the following document to learn more about
     our privacy policy.
   </p>
-  <h2 class="text-3xl font-extrabold tracking-tight text-white">
-    Privacy Policy
-  </h2>
-  <p class="mt-4 text-lg text-white">
-    Global Seafood Exchange LLC (GSE) is committed to protecting your privacy.
-    Please take a few minutes to read the following document to learn more about
-    our privacy policy.
-  </p>
-</div>
 
-<h2 class="text-3xl font-extrabold tracking-tight text-white">
-  Privacy Policy
-</h2>
-<p class="mt-4 text-lg text-white">
-  Global Seafood Exchange LLC (GSE) is committed to protecting your privacy.
-  Please take a few minutes to read the following document to learn more about
-  our privacy policy.
-</p>
-<h2 class="text-3xl font-extrabold tracking-tight text-white">
-  Privacy Policy
-</h2>
-<p class="mt-4 text-lg text-white">
-  Global Seafood Exchange LLC (GSE) is committed to protecting your privacy.
-  Please take a few minutes to read the following document to learn more about
-  our privacy policy.
-</p>
+  <ul>
+    <li>
+      information from your interaction with our Website and Services,
+      including, but not limited to, device ID, device type, geo-location
+      information, computer and connection information, statistics on page
+      views, traffic to and from www.globalseafoodexchange.com, referring URL,
+      IP address and standard web log information, all browsing activity, GSE
+      instant messenger content; and information we collect through cookies, web
+      beacons and similar technologies.
+    </li>
+    <li>information to conduct market research such as:</li>
+    <ul>
+      <li>demographics of our customers</li>
+      <li>all listing activity and data including but not limited to:</li>
+      <ul>
+        <li>order history</li>
+        <li>buy, sell and offer data</li>
+        <li>listing content and data</li>
+        <li>volume of goods purchased and sold</li>
+      </ul>
+      <li>instant messaging content</li>
+      <li>customer engagement</li>
+    </ul>
+  </ul>
+</div>
---
+++ src/routes/terms.svelte
@@ -0,0 +1,27 @@
+<script context="module">
+  import UnorderedList from "./UnorderedList.svelte";
+  export const prerender = true;
+</script>
+
+<svelte:head>
+  <title>Terms</title>
+</svelte:head>
+
+<div class="mt-6">
+  <h2 class="text-3xl font-extrabold tracking-tight text-white">
+    Privacy Policy
+  </h2>
+  <p class="mt-4 text-lg text-white">
+    Global Seafood Exchange LLC (GSE) is committed to protecting your privacy.
+    Please take a few minutes to read the following document to learn more about
+    our privacy policy.
+  </p>
+  <h2 class="text-3xl font-extrabold tracking-tight text-white">
+    Privacy Policy
+  </h2>
+  <p class="mt-4 text-lg text-white">
+    Global Seafood Exchange LLC (GSE) is committed to protecting your privacy.
+    Please take a few minutes to read the following document to learn more about
+    our privacy policy.
+  </p>
+</div>
