96f72a5ad07f001e854e917bc5428c5cd4111346
Jon Thornham <jon@thornham.ca>
2022-02-21 21:46:17 -0800

    Privacy Policy updated 3

--- src/routes/privacy.svelte
+++ src/routes/privacy.svelte
@@ -9,7 +9,7 @@
 
 <div class="mt-6 p-8">
   <h2 class="text-3xl font-extrabold tracking-tight text-white pt-8">
-    Privacy Policy
+    PRIVACY POLICY
   </h2>
   <p class="mt-4 text-lg text-white">
     Global Seafood Exchange LLC (GSE) is committed to protecting your privacy.
@@ -18,12 +18,39 @@
   </p>
 
   <h2 class="text-3xl font-extrabold tracking-tight text-white pt-8">
-    Privacy Policy
+    WHAT OUR PRIVACY POLICY COVERS
   </h2>
   <p class="mt-4 text-lg text-white">
-    Global Seafood Exchange LLC (GSE) is committed to protecting your privacy.
-    Please take a few minutes to read the following document to learn more about
-    our privacy policy.
+    This Privacy Policy covers how GSE treats the collection, disclosure and use
+    of any personal or business information received when you interact with GSE,
+    such as a visit to our website, when you contact us by phone or e-mail, or
+    when you using other GSE products and services. This may also include
+    information related to your past use. Personal or business information is
+    information that can identify you like your name, address, e-mail address,
+    banking information, geographic location and phone number. The policy will
+    inform you about the type of information we gather, how we use the
+    information and for what purpose, to whom we disclose the information, and
+    how we protect your information.
+  </p>
+
+  <h2 class="text-3xl font-extrabold tracking-tight text-white pt-8">
+    WHY WE COLLECT INFORMATION
+  </h2>
+  <p class="mt-4 text-lg text-white">
+    GSE collects information because it helps us deliver a greater level of
+    customer service. The information we receive allows us to provide more of
+    the products and services most popular to our customers. With your
+    information we can also keep you up-to-date on new features and products.
+  </p>
+
+  <h2 class="text-3xl font-extrabold tracking-tight text-white pt-8">
+    THE INFORMATION WE COLLECT AND HOW WE MAY USE IT
+  </h2>
+  <p class="mt-4 text-lg text-white">
+    Information we collect automatically: When you visit our Website or use GSE
+    products and services, we automatically collect the information sent to us
+    by your computer, mobile device or other equipment that provides access.
+    This information includes, but is not limited to:
   </p>
 
   <ul>
@@ -50,4 +77,209 @@
       <li>customer engagement</li>
     </ul>
   </ul>
+
+  <p class="mt-4 text-lg text-white">
+    Such analysis is carried out using aggregate and anonymous data.
+  </p>
+
+  <p class="mt-4 text-lg text-white">
+    Information you provide to us: We collect and store any information you
+    enter on our website and products and services. This information includes,
+    but is not limited to:
+  </p>
+
+  <ul>
+    <li>
+      information that you provide to us when you register for an account or for
+      the Services that you use;
+    </li>
+    <li>
+      additional information that you may provide to us through social media
+      sites or third-party Services;
+    </li>
+    <li>
+      information provided via correspondence through our Website or
+      correspondence that is sent to us;
+    </li>
+    <li>
+      information about your location and the location of your device, including
+      your device\'s unique identifier information if you have enabled this
+      service on your mobile device.
+    </li>
+  </ul>
+
+  <p class="mt-4 text-lg text-white">
+    Information from other sources: We may receive or collect additional
+    information about you from third parties and add this to our account
+    information. This information includes, but is not limited to:
+  </p>
+
+  <ul>
+    <li>demographic data;</li>
+    <li>navigation data;</li>
+    <li>
+      additional contact data and additional data about you from other sources,
+      such as public authorities, to the extent permitted by law.
+    </li>
+  </ul>
+
+  <p class="mt-4 text-lg text-white">
+    You agree that we may use your personal information for the following
+    purposes:
+  </p>
+
+  <ul>
+    <li>
+      to provide you access to our Services and Customer Support by means of
+      e-mail or telephone;
+    </li>
+    <li>
+      to prevent, detect and investigate potentially prohibited or illegal
+      activities, fraud and security breaches and to enforce our Terms of Use;
+    </li>
+    <li>to personalize, measure and improve our Services;</li>
+    <li>
+      to contact you, by e-mail, push notification, text message (SMS) or by
+      telephone, to inquire about our Services and updates or any other purposes
+      set forth in this Privacy Policy.
+    </li>
+  </ul>
+
+  <h2 class="text-3xl font-extrabold tracking-tight text-white pt-8">
+    WHEN GSE DISCLOSES OR SHARES YOUR PERSONAL INFORMATION
+  </h2>
+
+  <p class="mt-4 text-lg text-white">
+    GSE does not sell, rent or give your personal information to other people or
+    non-affiliated companies except when we have your permission or under the
+    following circumstances.
+  </p>
+
+  <p class="mt-4 text-lg text-white">
+    We may provide information to service providers, contracted by GSE such as,
+    database managers, accountants or lawyers, when it may be advantageous to
+    you. These companies work with or on behalf of GSE under confidentiality
+    agreements. They do not have the independent right to share your
+    information. These companies may use personal information to help us provide
+    more advanced web systems, manage and enhance a customer's experience, or
+    take steps to safeguard it in accordance with GSE's privacy policy.
+  </p>
+
+  <p class="mt-4 text-lg text-white">
+    At times it may be required by law or litigation, that GSE discloses
+    personal information. We respond to subpoenas, court orders, and legal
+    process. We may also use personal information to establish or exercise our
+    legal rights or defend ourselves against legal claims
+  </p>
+
+  <p class="mt-4 text-lg text-white">
+    GSE may disclose personal information in order to take legal action or
+    prevent illegal activities, situations involving potential threats to the
+    physical safety of any person, suspected fraud, violations of GSE's terms of
+    use, or as otherwise required by law.
+  </p>
+
+  <p class="mt-4 text-lg text-white">
+    In the event GSE is acquired or merged with another company we will transfer
+    your personal information. If this is to happen GSE will notify you before
+    your personal information is transferred becoming subject to a new privacy
+    policy.
+  </p>
+
+  <p class="mt-4 text-lg text-white">
+    GSE does not provide any personal information to marketers or advertisers.
+  </p>
+
+  <h2 class="text-3xl font-extrabold tracking-tight text-white pt-8">
+    COOKIES AND OTHER TECHNOLOGIES (WEBSITE SPECIFIC)
+  </h2>
+
+  <p class="mt-4 text-lg text-white">
+    As is true of most websites, GSE uses cookies and other technologies to help
+    us understand our customers. Using cookies and other technologies allows us
+    to determine which pages of our site are visited most frequently and the
+    amount of time spent on each page. We are able to study traffic patterns on
+    our website and the effectiveness of communications with our customers. This
+    allows us to provide an enhanced user experience for our customers by
+    providing new and/or updated content.
+  </p>
+
+  <p class="mt-4 text-lg text-white">
+    GSE collects this information using Google Analytics, which provides a
+    secure and effective method to gather information. Google Analytics uses
+    first party cookie and JavaScript code to collect information about
+    visitors. All of the information GSE and Google Analytics collects is done
+    so anonymously. Using Google Analytics we are able to track and compare our
+    advertising campaigns and email newsletters, identify which keywords
+    generate the most site traffic and conversions, understand what customers do
+    once they are on our site, identify where our customers are coming from and
+    where they are going on the web, study trends, and find out where our
+    customers are coming from geographically. For more information on Google
+    Analytics please search "Google Analytics" at www.google.com.
+  </p>
+
+  <p class="mt-4 text-lg text-white">
+    If you decide you would like to disable cookies for any reason, disable
+    cookies on your internet browser.
+  </p>
+
+  <h2 class="text-3xl font-extrabold tracking-tight text-white pt-8">
+    CHANGE TO GSE's PRIVACY POLICY
+  </h2>
+
+  <p class="mt-4 text-lg text-white">
+    GSE may from time to time update this privacy policy. You will be able to
+    find these updates on our website and within GSE products and services.
+  </p>
+
+  <h2 class="text-3xl font-extrabold tracking-tight text-white pt-8">
+    GSE'S COMMITMENT TO YOUR PRIVACY
+  </h2>
+
+  <p class="mt-4 text-lg text-white">
+    GSE is committed to protect your personal information. This policy is given
+    to our employees and strictly enforced to help safeguard your personal
+    information within the company. We will only allow reasonable access to
+    personal information to employees who we believe need to come in contact
+    with it to complete their jobs.
+  </p>
+
+  <h2 class="text-3xl font-extrabold tracking-tight text-white pt-8">
+    HOW GSE PROTECTS YOUR PERSONAL INFORMATION
+  </h2>
+
+  <p class="mt-4 text-lg text-white">
+    We protect your information by using technical and administrative security
+    measures (such as firewalls, data encryption, and administrative access
+    controls to the data and servers) that limit the risk of loss, abuse,
+    unauthorised access, disclosure, destruction and alteration.
+  </p>
+
+  <p class="mt-4 text-lg text-white">
+    You can also participate in keeping your personal information safe when you
+    are using GSE services. Please help by frequently changing your password
+    using both numbers and letters.
+  </p>
+
+  <h2 class="text-3xl font-extrabold tracking-tight text-white pt-8">
+    ABUSE OF GSE SERVICES
+  </h2>
+
+  <p class="mt-4 text-lg text-white">
+    Abuse and unsolicited commercial communications ("spam"): It is not allowed
+    to use our member-to-member communication resources to send spam or content
+    that violates our Terms of Use in any other way. For your security, we may
+    scan messages automatically and check for spam, viruses, phishing and other
+    malicious activity or illegal or prohibited content.
+  </p>
+
+  <h2 class="text-3xl font-extrabold tracking-tight text-white pt-8">
+    QUESTIONS ABOUT GSE'S PRIVACY POLICY
+  </h2>
+
+  <p class="mt-4 text-lg text-white">
+    If you have any questions or concerns regarding GSE’s privacy policy please
+    do not hesitate to contact us here. We will respond to your question as soon
+    as possible.
+  </p>
 </div>
