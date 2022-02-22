a321c4b39d009ef169885a39dd20d9dfb4577790
Jon Thornham <jon@thornham.ca>
2022-02-21 19:21:16 -0800

    Privacy Policy updated 2

--- src/routes/privacy.svelte
+++ src/routes/privacy.svelte
@@ -2,4 +2,57 @@
   <title>About</title>
 </svelte:head>
 
-<h1>Privacy Policy</h1>
+<div class="mt-6">
+  <h2 class="text-3xl font-extrabold tracking-tight text-white">
+    Privacy Policy
+  </h2>
+  <p class="mt-4 text-lg text-white">
+    Global Seafood Exchange LLC (GSE) is committed to protecting your privacy.
+    Please take a few minutes to read the following document to learn more about
+    our privacy policy.
+  </p>
+  <h2 class="text-3xl font-extrabold tracking-tight text-white">
+    Privacy Policy
+  </h2>
+  <p class="mt-4 text-lg text-white">
+    Global Seafood Exchange LLC (GSE) is committed to protecting your privacy.
+    Please take a few minutes to read the following document to learn more about
+    our privacy policy.
+  </p>
+</div>
+
+<div>
+  <h2 class="text-3xl font-extrabold tracking-tight text-white">
+    Privacy Policy
+  </h2>
+  <p class="mt-4 text-lg text-white">
+    Global Seafood Exchange LLC (GSE) is committed to protecting your privacy.
+    Please take a few minutes to read the following document to learn more about
+    our privacy policy.
+  </p>
+  <h2 class="text-3xl font-extrabold tracking-tight text-white">
+    Privacy Policy
+  </h2>
+  <p class="mt-4 text-lg text-white">
+    Global Seafood Exchange LLC (GSE) is committed to protecting your privacy.
+    Please take a few minutes to read the following document to learn more about
+    our privacy policy.
+  </p>
+</div>
+
+<h2 class="text-3xl font-extrabold tracking-tight text-white">
+  Privacy Policy
+</h2>
+<p class="mt-4 text-lg text-white">
+  Global Seafood Exchange LLC (GSE) is committed to protecting your privacy.
+  Please take a few minutes to read the following document to learn more about
+  our privacy policy.
+</p>
+<h2 class="text-3xl font-extrabold tracking-tight text-white">
+  Privacy Policy
+</h2>
+<p class="mt-4 text-lg text-white">
+  Global Seafood Exchange LLC (GSE) is committed to protecting your privacy.
+  Please take a few minutes to read the following document to learn more about
+  our privacy policy.
+</p>
