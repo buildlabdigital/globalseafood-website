471f204bfca76bd38ad952e2688517e088e6b5df
Mike Williams <mwilliams1188@gmail.com>
2022-02-21 20:24:56 -0500

    try add privacy hidden link

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,6 +2,7 @@
   <div
     class="mx-auto max-w-md py-12 px-4 overflow-hidden sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8"
   >
+    <a href="/privacy" class="hidden">Privacy</a>
     <!-- <nav class="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
       <div class="px-5 py-2">
         <a href="#" class="text-base text-gray-400 hover:text-gray-300">
